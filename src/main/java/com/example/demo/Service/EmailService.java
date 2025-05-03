c11b914e1c076114745c91413aaf1dbb706585b2
kanishka <kanishka.pramar@gdn-commerce.com>
2025-05-04 02:26:00 +0530

    Email MicroService

---
+++ src/main/java/com/example/demo/Service/EmailService.java
@@ -0,0 +1,24 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendCheckoutEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom("krishnavineeth0304@gmail.com");  // Use your own email here
+
+        mailSender.send(message);
+        System.out.println("Email sent successfully.");
+    }
+}
